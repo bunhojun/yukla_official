46aa4cf7ccd78ae560f2157709126fc8ef1efcaa
Hojun Bun <ponnponn.p@gmail.com>
2020-10-29 10:40:23 +0900

    improve accessibility

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 const common = require('./webpack.common');
 
 const ASSET_PATH = process.env.ASSET_PATH || '/';
@@ -14,6 +15,9 @@ module.exports = merge(common, {
     publicPath: ASSET_PATH,
   },
   plugins: [
+    new CompressionPlugin({
+      algorithm: 'gzip',
+    }),
     new MiniCssExtractPlugin({ filename: '[name].css' }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
