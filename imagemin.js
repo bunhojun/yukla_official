96c3134028c133edec3933da020686a94181ffe5
Hojun Bun <ponnponn.p@gmail.com>
2021-04-29 15:44:39 +0900

    add comment to imagemin

--- imagemin.js
+++ imagemin.js
@@ -2,6 +2,12 @@ const imagemin = require('imagemin');
 const imageminJpegoptim = require('imagemin-jpegoptim');
 const imageminPngquant = require('imagemin-pngquant');
 
+/**
+ * node imagemin -> minify images in the image directory
+ * node imagemin warned -> minify images in the warned_images directory
+ * note: warned images = images that are so big
+ *       that can't be minified the same way normal images get
+ */
 (async () => {
   const isTooBig = process.argv[2] && process.argv[2] === 'warned';
   const quality = isTooBig ? 15 : 30;
